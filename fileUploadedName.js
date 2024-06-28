7b13b4aadd13e1957dd1a79906b0f78d11ea331e
Emir Gegic <gegeeem@gmail.com>
2024-06-28 11:59:46 +0200

    adding type of file icon

--- fileUploadedName.js
+++ fileUploadedName.js
@@ -1,12 +1,24 @@
 import {showHide} from "./showHideElement.js"
-function  displayFileUploadedName(idOfInpuFile, idSpan){
+function  displayFileUploadedName(idOfInpuFile, idSpan, displayFileType){
     const file = document.querySelector(idOfInpuFile);
     const getSpanDisplayFileName = document.querySelector(idSpan)
+    
+   
+    
 
 
     const getFileUploadedNam = ()=>{
-        getSpanDisplayFileName.innerHTML = file.files[0].name;
+        getSpanDisplayFileName.innerHTML = file.files[0].name.slice(0,8)+"...";
         console.log("ime fajla", file.files[0].name)
+        console.log("type",file.files[0].type)
+         //prikazi tip fajla na ikoni
+        const getIconFileToDisplayType = document.querySelector("#fileExtension")
+        const fileType = file.files[0].type;// file type, example "image/png"
+        const dashIndex = file.files[0].type.indexOf("/")// find index of dash "/"
+        const extensionFile = fileType.slice(dashIndex + 1, dashIndex + 5)// slice string and get png
+        console.log(extensionFile)
+        getIconFileToDisplayType.textContent = extensionFile;
+
         showHide("show","#resetDiv"); // prikazi "ponisti" dugme
         showHide("show","#startEncryption") // prikazi "Pokreni enkripciju dugme"
         showHide("show", "#btnForDec")
@@ -16,7 +28,7 @@ function  displayFileUploadedName(idOfInpuFile, idSpan){
     file.addEventListener("change",getFileUploadedNam)
 }
 
-displayFileUploadedName("#textForEncFile", "#fileUploadedName")
+displayFileUploadedName("#textForEncFile", "#displayNameOfUploadedFile")
 displayFileUploadedName("#textForDecFile","#encryptedFileUploadedName")
 displayFileUploadedName("#dataForDec","#dataForDecryption")
 
