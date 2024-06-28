3728b87339e87d4160075a69377c1c9a8eed2fc3
Emir Gegic <gegeeem@gmail.com>
2024-06-28 12:21:03 +0200

    do we

--- fileUploadedName.js
+++ fileUploadedName.js
@@ -8,17 +8,19 @@ function  displayFileUploadedName(idOfInpuFile, idSpan, displayFileType){
 
 
     const getFileUploadedNam = ()=>{
-        getSpanDisplayFileName.innerHTML = file.files[0].name.slice(0,8)+"...";
-        console.log("ime fajla", file.files[0].name)
-        console.log("type",file.files[0].type)
+        getSpanDisplayFileName.innerHTML = file.files[0].name.slice(0,15)+"...";
          //prikazi tip fajla na ikoni
-        const getIconFileToDisplayType = document.querySelector("#fileExtension")
-        const fileType = file.files[0].type;// file type, example "image/png"
-        const dashIndex = file.files[0].type.indexOf("/")// find index of dash "/"
-        const extensionFile = fileType.slice(dashIndex + 1, dashIndex + 5)// slice string and get png
-        console.log(extensionFile)
-        getIconFileToDisplayType.textContent = extensionFile;
+        const getIconFileToDisplayType = document.querySelector(displayFileType)
+        if(getIconFileToDisplayType){
+            const fileType = file.files[0].type;// file type, example "image/png"
+            const dashIndex = file.files[0].type.indexOf("/")// find index of dash "/"
+            const extensionFile = fileType.slice(dashIndex + 1, dashIndex + 5)// slice string and get png
+            getIconFileToDisplayType.textContent = extensionFile; // dodeli format "png"
 
+        }
+       
+
+        showHide("hide","#btnIzaberiFajl")
         showHide("show","#resetDiv"); // prikazi "ponisti" dugme
         showHide("show","#startEncryption") // prikazi "Pokreni enkripciju dugme"
         showHide("show", "#btnForDec")
@@ -28,7 +30,7 @@ function  displayFileUploadedName(idOfInpuFile, idSpan, displayFileType){
     file.addEventListener("change",getFileUploadedNam)
 }
 
-displayFileUploadedName("#textForEncFile", "#displayNameOfUploadedFile")
+displayFileUploadedName("#textForEncFile", "#displayNameOfUploadedFile","#fileExtension")
 displayFileUploadedName("#textForDecFile","#encryptedFileUploadedName")
 displayFileUploadedName("#dataForDec","#dataForDecryption")
 
