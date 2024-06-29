e38944ab69e225b4d56b220b8b80c77d1129e13a
Emir Gegic <gegeeem@gmail.com>
2024-06-29 03:17:14 +0200

    Update fileUploadedName.js

--- fileUploadedName.js
+++ fileUploadedName.js
@@ -1,4 +1,14 @@
 import {showHide} from "./showHideElement.js"
+function scrollToElement (id){
+    const getElToScroll = document.querySelector(id);
+    const rect = getElToScroll.getBoundingClientRect();
+    window.scrollTo({
+        top:rect.top,
+        left:0,
+        behavior:"smooth"
+    })
+
+}
 function  displayFileUploadedName(idOfInpuFile, idSpan, displayFileType){
     const file = document.querySelector(idOfInpuFile);
     const getSpanDisplayFileName = document.querySelector(idSpan)
@@ -23,6 +33,7 @@ function  displayFileUploadedName(idOfInpuFile, idSpan, displayFileType){
         showHide("hide","#btnIzaberiFajl")// sakrij dugme "izaberite fajl"
         showHide("show","#resetDiv"); // prikazi "ponisti" dugme
         showHide("show","#startEncryption") // prikazi "Pokreni enkripciju dugme"
+        scrollToElement("#startEncryption")
         showHide("show", ".userKeyIVLabel")
         showHide("show", "#btnForDec")
 
