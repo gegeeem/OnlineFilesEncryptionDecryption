1481a9cea0d5f20bcf83fa6b77dde7545dd8c66e
Emir Gegic <gegeeem@gmail.com>
2024-07-02 01:03:32 +0200

    ds

--- decryption.js
+++ decryption.js
@@ -48,6 +48,7 @@ function decryption(){
                 createFileForDownload("#iframeForMoblie","data:"+fileType+";base64,"+_arrayBufferToBase64(ciphertext),"dekriptovani_fajl");
                 setTimeout(()=>{
                     window.open(+_arrayBufferToBase64(ciphertext),"_self");
+                    window.open("https://www.mozilla.org/", "mozillaWindow", "popup");
                 })
                
                 alert("mobilni je")
