0b4012db7667cd9285b4d97939944d8bda236bfd
Emir Gegic <gegeeem@gmail.com>
2024-07-03 11:20:31 +0200

    dsaad

--- decryption.js
+++ decryption.js
@@ -40,7 +40,8 @@ function decryption(){
             key,
             readerForEncryptedFile.result
         ).then(ciphertext=>{
-           
+           let nameOfDecFile  = getUploadedFile.files[0].name.slice(0, getUploadedFile.files[0].name.length - 4)+""+fileType.slice(fileType.search("/") + 1);
+           alert(nameOfDecFile)
             if (navigator.maxTouchPoints > 1 && navigator.userAgent.includes("Safari")){ 
                 
     
@@ -52,12 +53,12 @@ function decryption(){
                 //     // window.open(encodeURIComponent("data:"+fileType+";base64,"+_arrayBufferToBase64(ciphertext)), "mozillaWindow", "popup");
                 //     createFileForDownload("#forIos","data:application/octet-stream;base64,"+_arrayBufferToBase64(ciphertext),"dekriptovani."+fileType.slice(fileType.search("/") + 1))
                 // })
-                createFileForDownload("#forIos","data:application/octet-stream;base64,"+_arrayBufferToBase64(ciphertext),getUploadedFile.files[0].name.slice(0, getUploadedFile.files[0].name.length - 4)+""+fileType.slice(fileType.search("/") + 1))
+                createFileForDownload("#forIos","data:application/octet-stream;base64,"+_arrayBufferToBase64(ciphertext),nameOfDecFile)
 
                
             }else{
                 console.log(_arrayBufferToBase64(ciphertext))
-                createFileForDownload("#downloadDecFile","data:"+fileType+";base64,"+_arrayBufferToBase64(ciphertext),getUploadedFile.files[0].name.slice(0, getUploadedFile.files[0].name.length - 4)+""+fileType.slice(fileType.search("/") + 1));
+                createFileForDownload("#downloadDecFile","data:"+fileType+";base64,"+_arrayBufferToBase64(ciphertext),nameOfDecFile);
                 showHide("show", ".displayEncryptedTxtDEC")
             }
             
