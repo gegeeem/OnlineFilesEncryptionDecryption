3172b8d5a9b944dc0978c80c65c5ebc642d053af
Emir Gegic <gegeeem@gmail.com>
2024-07-11 00:22:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ import {dispalyExstdKeyFileIconAndName} from "./displayExtdKeyFile.js"
 
 const getExtdDataForEnc = document.querySelector("#jwk");// dugme za ucitavanje postojeceg kljuca
 const getResetBtnEnc = document.querySelector("#rstBtn");// dugme "ponisti"
-const numOfThreads = window.navigator.hardwareConcurrency;
-alert("broj niti je: "+numOfThreads);
-console.log(numOfThreads);
+// const numOfThreads = window.navigator.hardwareConcurrency;
+// alert("broj niti je: "+numOfThreads);
+// console.log(numOfThreads);
 function handleResetBtn(){
     showHide("show","#btnIzaberiFajl")// prikazi dugme "izaberite fajl" nakon sto je pritisnuto "ponisti"
     showHide("hide","#fileUploadedName")//sakrij ime fajla
