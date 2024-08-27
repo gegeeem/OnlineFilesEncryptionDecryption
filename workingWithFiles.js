7f431ac4bab8a95bdc66c8805d92d4a91e7183ea
Emir Gegic <gegeeem@gmail.com>
2024-08-27 16:16:58 +0200

    settimout

--- workingWithFiles.js
+++ workingWithFiles.js
@@ -5,6 +5,9 @@ export function createFileForDownload(id, file, name){
         // getLink.src = file
         getLink.href = file
         getLink.download = name;
-       getLink.addEventListener("click",()=>{URL.revokeObjectURL(getLink.href)});
+//        getLink.addEventListener("click",()=>{URL.revokeObjectURL(getLink.href)});
+       setTimeout(function(){ window.URL.revokeObjectURL(url); }, 3000)
+       getLink.addEventListener("click",()=>{ setTimeout(function(){ window.URL.revokeObjectURL(getLink.href); }, 3000)});
+
     
 }
